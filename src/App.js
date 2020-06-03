dffd37697ef39df6de45cd45aa032dd96a9e52f6
Adrien Chantrel <adrien.chantrel@isen.yncrea.fr>
2020-06-03 17:26:56 +0200

    Added google connection - Still have to get user infos from code sent by Google and send them to the backend

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import { GoogleSigninButton } from './Components/GoogleSigninButton/googleSigninButton';
 
 function App() {
   return (
     <div className="App">
-     
+      <GoogleSigninButton/>
     </div>
   );
 }
