8b8fedb9e3c4adf38d27d5af181d71e22c127d20
François LOSFELD <Francois>
2020-06-11 16:16:44 +0200

    First version - problem with fetch request

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import {AirQualityWidget} from './components/airQualityWidget/AirQualityWidget';
 
 function App() {
   return (
     <div className="App">
+
+      <AirQualityWidget/>
      
     </div>
   );
